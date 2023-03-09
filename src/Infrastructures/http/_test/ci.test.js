a4506a37eeafba24d192b770e6d95edecebda2fa
elSyarif <id.syarif@gmail.com>
2023-03-09 13:19:47 +0700

    add: say hello world test case feature

---
+++ src/Infrastructures/http/_test/ci.test.js
@@ -0,0 +1,19 @@
+const createServer = require('../createServer');
+
+describe('when GET /', () => {
+  it('should return 200 and Hello World', async () => {
+    // arrange
+    const server = createServer({});
+
+    // action
+    const response = await server.inject({
+      method: 'GET',
+      url: '/',
+    });
+
+    // assert
+    const responseJson = JSON.parse(response.payload);
+    expect(response.statusCode).toEqual(200);
+    expect(responseJson.value).toEqual('Hello World!');
+  });
+});
